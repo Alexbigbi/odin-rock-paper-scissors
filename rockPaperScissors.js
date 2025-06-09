2ed52d691de31801a07dc7c69b5943ccc2598e9a
Alexbigbi <122051653+Alexbigbi@users.noreply.github.com>
2025-06-09 12:33:43 -0600

    Write the logic to play a single round
    This commit adds the rules and logic for the game, increasing score
    if either side wins.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -14,7 +14,7 @@ function getComputerChoice() {
 
     //create a variable that stores a random number between 0 and 2
     let randomNumber = Math.floor(Math.random() * 3);
-    console.log(randomNumber);
+    //console.log(randomNumber);
     //if variable is 0, return rock
         //if variable is 1, return paper
             //if variable is 2, return scissors
@@ -23,17 +23,62 @@ function getComputerChoice() {
     paper : scissors;
 };
 //check if our function is working
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 
 //create a function named getHumanChoice
 function getHumanChoice() {
     //prompt the user to choose their move
     let choice = prompt("What's your move?");
     return choice;
-}
+};
 
 //test if our function is working
-console.log(getHumanChoice());
+//console.log(getHumanChoice());
+
+//create two variables that track human and computer score
+//initialize both variables to 0
+let humanScore = 0;
+let computerScore = 0;
+
+//create a function for the game that takes human and computer choices as arguments
+function playRound(humanChoice, computerChoice) {
+
+    //make human choice case insensitive
+    let humanChoiceInsensitive = humanSelection.toLowerCase();
+
+    if (humanChoiceInsensitive === computerChoice) {
+        console.log("It's a tie!");
+    }
+    //write the cases for when the human wins
+    else if (humanChoiceInsensitive == "rock" && computerChoice == "scissors") {
+        console.log("You win! Rock beats Scissors.");
+        humanScore++;
+    }
+    else if (humanChoiceInsensitive == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper beats Rock.");
+        humanScore++;
+    }
+    else if (humanChoiceInsensitive == "scissors" && computerChoice == "paper") {
+        console.log("You win! Scissors beat Paper.");
+        humanScore++;
+    }
+    else {
+        console.log("You lose! " + humanChoice + " loses to " + computerChoice);
+        computerScore++;
+    }
+}
+    //create the logic for the game. if same choice = tie and no score change
+    //if rock vs scissors, rock wins, adds one point
+    //if paper vs rock, paper wins, adds one point
+    //if scissors vs paper, scissors wins, adds one point
+    //display to console the result of the round
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+console.log(humanSelection);
+console.log(computerSelection);
+
+playRound(humanSelection, computerSelection);
 
 
 
